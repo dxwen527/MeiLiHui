d7f8476d6129c303f08cbf1b3558dc638fd68138
sevenasdf <1924167890@qq.com>
2018-10-31 01:14:18 +0800

    revise Sowing map

--- static/JS/homepage.js
+++ static/JS/homepage.js
@@ -1,9 +1,18 @@
 $(function () {
-    $('img').each(function () {
-        var imgpath = $(this).attr('src')
-        imgpath = "{% static '" + imgpath +  "' %}"
-        $(this).attr('src', imgpath)
-    })
+    var swiper = new Swiper('.swiper-container', {
+        // navigation: {
+        //   // nextEl: '.swiper-button-next',
+        //   // prevEl: '.swiper-button-prev',
+        //
+        // },
+        // spaceBetween: 30,
 
-    console.log($('body').html())
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            autoplay: 3000,
+            slidesPerView: 1,
+            loop: true,
+        },
+    });
 })
